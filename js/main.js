99b3a00b0eb360e113c04aa555d8373a78ce279a
Kieran McFarlane <kieran@verbbrands.com>
2017-02-04 22:11:15 +0000

    added bowser-jr and other pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function(){
 
-	$('#H2_42').t('Ranking <del>Super Smash Bros.</del> Smash 4 Table for <mark class="pink">VERB</mark>',{speed:45,speed_vary:true});
+	$('#H2_42').t('<del>Super Smash Bros. for Wii U</del> <del>Super</del> Smash 4 Ranking Table for <mark class="pink">VERB</mark>',{speed:45,speed_vary:true});
+	$('#H2_43').t('<del>Super Smash Bros. for Wii U</del> <del>Super</del> Smash 4 Ranking Match-ups for <mark class="pink">VERB</mark>',{speed:45,speed_vary:true});
+
 });
